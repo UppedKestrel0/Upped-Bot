2e5e73cd28838df11a5989cdba34c6b164522588
UppedKestrel0 <37655602+UppedKestrel0@users.noreply.github.com>
2019-04-16 15:58:06 +1000

    Update changelog.js

--- commands/changelog.js
+++ commands/changelog.js
@@ -2,14 +2,12 @@ const Discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
 
-    let priority = args[1];
-    let change = message.slice(priority).join(" ");
+    let change = args.slice(0).join(" ");
 
     let changelogEmbed = new Discord.RichEmbed()
     .setTitle("**NEW LOG**")
     .setColor("#f1f433")
-    .addField("Change", change)
-    .addField("Priority", priority);
+    .addField("Change", change);
 
     let changelogChannel = message.guild.channels.find(`name`, "changelog");
     message.delete().catch(O_o=> {});
