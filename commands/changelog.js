fcf668fc411008e60a5e2059ccc1a37041b95815
UppedKestrel0 <37655602+UppedKestrel0@users.noreply.github.com>
2019-04-16 15:25:45 +1000

    New Command!

---
+++ commands/changelog.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) => {
+
+    let priority = args.slice(1).join(" ");
+    let change = args.slice(0).join(" ");
+
+    let changelogEmbed = new Discord.RichEmbed()
+    .setTitle("**NEW LOG**")
+    .setColor("#f1f433")
+    .addField("Change", change)
+    .addField("Priority", priority);
+
+};
+
+module.exports.help = {
+    name: "changelog"
+}
