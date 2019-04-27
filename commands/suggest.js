85ecde79428e78674e7d51aeb21b9da667467186
UppedKestrel0 <37655602+UppedKestrel0@users.noreply.github.com>
2019-04-27 13:11:09 +1000

    Added Reactions back to Suggest

--- commands/suggest.js
+++ commands/suggest.js
@@ -3,8 +3,8 @@ const discord = require("discord.js");
 module.exports.run = async(bot, message, args) => {
 
     let suggestion = args.slice(0).join(" ");
-    let reactionA = message.guild.emojis.get("571485378993717249");
-    let reactionB = message.guild.emojis.get("571485462838116353");
+    let reactionA = message.guild.emojis.get("533967580227174412");
+    let reactionB = message.guild.emojis.get("534686684273442826");
 
     let suggestionEmbed = new discord.RichEmbed()
     .setTitle("**NEW SUGGESTION**")
