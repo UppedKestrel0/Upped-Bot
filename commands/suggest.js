122d0d5b6f7d1b52ca01beb652e15c0c082009d3
UppedKestrel0 <37655602+UppedKestrel0@users.noreply.github.com>
2019-04-27 13:06:47 +1000

    Fixed Commands

--- commands/suggest.js
+++ commands/suggest.js
@@ -3,12 +3,12 @@ const discord = require("discord.js");
 module.exports.run = async(bot, message, args) => {
 
     let suggestion = args.slice(0).join(" ");
-    let reactionA = message.guild.emojis.get("533967580227174412");
-    let reactionB = message.guild.emojis.get("534686684273442826");
+    let reactionA = message.guild.emojis.get("571485378993717249");
+    let reactionB = message.guild.emojis.get("571485462838116353");
 
     let suggestionEmbed = new discord.RichEmbed()
     .setTitle("**NEW SUGGESTION**")
-    .setColor(yellow)
+    .setColor("#ffff00")
     .addField("__Suggestion__", suggestion)
     .addField("__Author__", message.author);
 
